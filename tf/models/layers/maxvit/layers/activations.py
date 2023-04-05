e3f81e72668dff6483f71363b900e375ef368337
JVD9kh96 <J.khorramdel96@gmail.com>
2023-04-05 21:28:40 +0000

    activations.py functions have been replaced with tf implementation

--- tf/models/layers/maxvit/layers/activations.py
+++ tf/models/layers/maxvit/layers/activations.py
@@ -9,79 +9,78 @@ Hacked together by / Copyright 2020 Ross Wightman
 import torch
 from torch import nn as nn
 from torch.nn import functional as F
+import tensorflow as tf
 
 
 def swish(x, inplace: bool = False):
     """Swish - Described in: https://arxiv.org/abs/1710.05941
     """
-    return x.mul_(x.sigmoid()) if inplace else x.mul(x.sigmoid())
+    return tf.math.multiply(x, tf.nn.sigmoid(x))
 
-
-class Swish(nn.Module):
-    def __init__(self, inplace: bool = False):
+class Swish(tf.keras.layers.Layer):
+    def __init__(self, inplace: bool=False):
         super(Swish, self).__init__()
         self.inplace = inplace
-
-    def forward(self, x):
+    def call(self, x):
         return swish(x, self.inplace)
+
 
 
 def mish(x, inplace: bool = False):
     """Mish: A Self Regularized Non-Monotonic Neural Activation Function - https://arxiv.org/abs/1908.08681
     NOTE: I don't have a working inplace variant
     """
-    return x.mul(F.softplus(x).tanh())
+    return tf.math.multiply(x, tf.math.tanh(tf.math.softplus(x)))
 
-
-class Mish(nn.Module):
+class Mish(tf.keras.layers.Layer):
     """Mish: A Self Regularized Non-Monotonic Neural Activation Function - https://arxiv.org/abs/1908.08681
     """
     def __init__(self, inplace: bool = False):
         super(Mish, self).__init__()
 
-    def forward(self, x):
+    def call(self, x):
         return mish(x)
 
 
 def sigmoid(x, inplace: bool = False):
-    return x.sigmoid_() if inplace else x.sigmoid()
+    return tf.nn.sigmoid(x)
 
 
 # PyTorch has this, but not with a consistent inplace argmument interface
-class Sigmoid(nn.Module):
+class Sigmoid(tf.keras.layers.Layer):
     def __init__(self, inplace: bool = False):
         super(Sigmoid, self).__init__()
         self.inplace = inplace
 
-    def forward(self, x):
-        return x.sigmoid_() if self.inplace else x.sigmoid()
+    def call(self, x):
+        return sigmoid(x, self.inplace)
 
 
 def tanh(x, inplace: bool = False):
-    return x.tanh_() if inplace else x.tanh()
+    return tf.nn.tanh()
 
 
-# PyTorch has this, but not with a consistent inplace argmument interface
-class Tanh(nn.Module):
+class Tanh(tf.keras.layers.Layer):
     def __init__(self, inplace: bool = False):
         super(Tanh, self).__init__()
         self.inplace = inplace
 
-    def forward(self, x):
-        return x.tanh_() if self.inplace else x.tanh()
+    def call(self, x):
+        return tanh(x, self.inplace)
 
 
 def hard_swish(x, inplace: bool = False):
     inner = F.relu6(x + 3.).div_(6.)
-    return x.mul_(inner) if inplace else x.mul(inner)
+    inner = tf.math.scalar_mul(1./6., tf.nn.relu6(x + 3.))
+    return tf.math.multiply(inner, x)
 
 
-class HardSwish(nn.Module):
+class HardSwish(tf.keras.layers.Layer):
     def __init__(self, inplace: bool = False):
         super(HardSwish, self).__init__()
         self.inplace = inplace
 
-    def forward(self, x):
+    def call(self, x):
         return hard_swish(x, self.inplace)
 
 
@@ -92,12 +91,12 @@ def hard_sigmoid(x, inplace: bool = False):
         return F.relu6(x + 3.) / 6.
 
 
-class HardSigmoid(nn.Module):
+class HardSigmoid(tf.keras.layers.Layer):
     def __init__(self, inplace: bool = False):
         super(HardSigmoid, self).__init__()
         self.inplace = inplace
 
-    def forward(self, x):
+    def call(self, x):
         return hard_sigmoid(x, self.inplace)
 
 
@@ -106,40 +105,36 @@ def hard_mish(x, inplace: bool = False):
     Experimental, based on notes by Mish author Diganta Misra at
       https://github.com/digantamisra98/H-Mish/blob/0da20d4bc58e696b6803f2523c58d3c8a82782d0/README.md
     """
-    if inplace:
-        return x.mul_(0.5 * (x + 2).clamp(min=0, max=2))
-    else:
-        return 0.5 * x * (x + 2).clamp(min=0, max=2)
+    return tf.scalar_mul(0.5, tf.math.multiply(x, tf.clip_by_value(x + 2.0, 0.0, 2.0)))
 
-
-class HardMish(nn.Module):
+class HardMish(tf.keras.layers.Layer):
     def __init__(self, inplace: bool = False):
         super(HardMish, self).__init__()
         self.inplace = inplace
 
-    def forward(self, x):
+    def call(self, x):
         return hard_mish(x, self.inplace)
 
 
-class PReLU(nn.PReLU):
+class PReLU(tf.keras.layers.Layer):
     """Applies PReLU (w/ dummy inplace arg)
     """
     def __init__(self, num_parameters: int = 1, init: float = 0.25, inplace: bool = False) -> None:
         super(PReLU, self).__init__(num_parameters=num_parameters, init=init)
-
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return F.prelu(input, self.weight)
-
-
-def gelu(x: torch.Tensor, inplace: bool = False) -> torch.Tensor:
-    return F.gelu(x)
+        self.prelu = tf.keras.layers.PReLU()
+    def call(self, x):
+        return self.prelu(x)
 
 
-class GELU(nn.Module):
+def gelu(x, inplace: bool = False):
+    return tf.nn.gelu(x)
+
+
+class GELU(tf.keras.layers.Layer):
     """Applies the Gaussian Error Linear Units function (w/ dummy inplace arg)
     """
     def __init__(self, inplace: bool = False):
         super(GELU, self).__init__()
 
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return F.gelu(input)
+    def call(self, x):
+        return gelu(x)
