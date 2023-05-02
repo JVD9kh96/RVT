1d490bc31403134ed8946e0ac650440bb478eb49
Javad <54026703+JVD9kh96@users.noreply.github.com>
2023-05-02 20:51:42 +0330

    Update dataset_rnd.py

--- tf/data/genx_utils/dataset_rnd.py
+++ tf/data/genx_utils/dataset_rnd.py
@@ -5,8 +5,6 @@ from typing import List
 
 import numpy as np
 from omegaconf import DictConfig
-from torch.utils.data import ConcatDataset, Dataset
-from torch.utils.data.sampler import WeightedRandomSampler
 from tqdm import tqdm
 
 from data.genx_utils.labels import SparselyBatchedObjectLabels
@@ -15,11 +13,15 @@ from data.utils.augmentor import RandomSpatialAugmentorGenX
 from data.utils.types import DatasetMode, LoaderDataDictGenX, DatasetType, DataType
 
 
-class SequenceDataset(Dataset):
+import numpy as np
+import tensorflow as tf
+
+class SequenceDataset(tf.keras.utils.Sequence):
     def __init__(self,
                  path: Path,
-                 dataset_mode: DatasetMode,
-                 dataset_config: DictConfig):
+                 dataset_mode: str,
+                 dataset_config: DictConfig,
+                 batch_size: int):
         assert path.is_dir()
 
         ### extract settings from config ###
@@ -49,7 +51,7 @@ class SequenceDataset(Dataset):
                                                 only_load_end_labels=only_load_end_labels)
 
         self.spatial_augmentor = None
-        if dataset_mode == DatasetMode.TRAIN:
+        if dataset_mode == 'train':
             resolution_hw = tuple(dataset_config.resolution_hw)
             assert len(resolution_hw) == 2
             ds_by_factor_2 = dataset_config.downsample_by_factor_2
@@ -60,38 +62,46 @@ class SequenceDataset(Dataset):
                 automatic_randomization=True,
                 augm_config=augm_config.random)
 
-    def only_load_labels(self):
-        self.sequence.only_load_labels()
-
-    def load_everything(self):
-        self.sequence.load_everything()
+        self.batch_size = batch_size
 
     def __len__(self):
-        return len(self.sequence)
+        return len(self.sequence) // self.batch_size
+
+    def on_epoch_end(self):
+        self.sequence.shuffle()
 
     def __getitem__(self, index: int) -> LoaderDataDictGenX:
-
-        item = self.sequence[index]
+        start_idx = index * self.batch_size
+        end_idx = (index + 1) * self.batch_size
+        items = self.sequence[start_idx:end_idx]
 
         if self.spatial_augmentor is not None and not self.sequence.is_only_loading_labels():
-            item = self.spatial_augmentor(item)
+            items = [self.spatial_augmentor(item) for item in items]
 
-        return item
+        return items
 
 
-class CustomConcatDataset(ConcatDataset):
-    datasets: List[SequenceDataset]
-
+class CustomConcatDataset(tf.keras.utils.Sequence):
     def __init__(self, datasets: Iterable[SequenceDataset]):
-        super().__init__(datasets=datasets)
+        self.datasets = datasets
 
-    def only_load_labels(self):
-        for idx, dataset in enumerate(self.datasets):
-            self.datasets[idx].only_load_labels()
+        self.num_samples = 0
+        self.cumulative_sizes = [0]
+        for dataset in self.datasets:
+            self.num_samples += len(dataset)
+            self.cumulative_sizes.append(self.num_samples)
 
-    def load_everything(self):
-        for idx, dataset in enumerate(self.datasets):
-            self.datasets[idx].load_everything()
+    def __len__(self):
+        return self.num_samples
+
+    def on_epoch_end(self):
+        for dataset in self.datasets:
+            dataset.on_epoch_end()
+
+    def __getitem__(self, index: int):
+        dataset_idx = np.searchsorted(self.cumulative_sizes, index, side='right') - 1
+        sample_idx = index - self.cumulative_sizes[dataset_idx]
+        return self.datasets[dataset_idx][sample_idx]
 
 
 def build_random_access_dataset(dataset_mode: DatasetMode, dataset_config: DictConfig) -> CustomConcatDataset:
@@ -112,23 +122,22 @@ def build_random_access_dataset(dataset_mode: DatasetMode, dataset_config: DictC
     return CustomConcatDataset(seq_datasets)
 
 
-def get_weighted_random_sampler(dataset: CustomConcatDataset) -> WeightedRandomSampler:
-    class2count = dict()
-    ClassAndCount = namedtuple('ClassAndCount', ['class_ids', 'counts'])
-    classandcount_list = list()
+def get_weighted_random_sampler(dataset):
+    class2count = {}
+    classandcount_list = []
     print('--- START generating weighted random sampler ---')
     dataset.only_load_labels()
-    for idx, data in enumerate(tqdm(dataset, desc='iterate through dataset')):
-        labels: SparselyBatchedObjectLabels = data[DataType.OBJLABELS_SEQ]
+    for idx, data in enumerate(tf.data.Dataset.as_numpy_iterator(dataset)):
+        labels = data[DataType.OBJLABELS_SEQ]
         label_list, valid_batch_indices = labels.get_valid_labels_and_batch_indices()
-        class_ids_seq = list()
+        class_ids_seq = []
         for label in label_list:
             class_ids_numpy = np.asarray(label.class_id.numpy(), dtype='int32')
             class_ids_seq.append(class_ids_numpy)
         class_ids_seq, counts_seq = np.unique(np.concatenate(class_ids_seq), return_counts=True)
         for class_id, count in zip(class_ids_seq, counts_seq):
             class2count[class_id] = class2count.get(class_id, 0) + count
-        classandcount_list.append(ClassAndCount(class_ids=class_ids_seq, counts=counts_seq))
+        classandcount_list.append((class_ids_seq, counts_seq))
     dataset.load_everything()
 
     class2weight = {}
@@ -139,11 +148,11 @@ def get_weighted_random_sampler(dataset: CustomConcatDataset) -> WeightedRandomS
     weights = []
     for classandcount in classandcount_list:
         weight = 0
-        for class_id, count in zip(classandcount.class_ids, classandcount.counts):
+        for class_id, count in zip(classandcount[0], classandcount[1]):
             # Not only weight depending on class but also depending on number of occurrences.
             # This will bias towards sampling "frames" with more bounding boxes.
             weight += class2weight[class_id] * count
         weights.append(weight)
 
     print('--- DONE generating weighted random sampler ---')
-    return WeightedRandomSampler(weights=weights, num_samples=len(weights), replacement=True)
+    return tf.data.experimental.rejection_resample(dataset, weights=weights, target_prob=1/len(weights), seed=None, reshuffle_each_iteration=True, window_size=None, variable_window_size=False, name=None)    
